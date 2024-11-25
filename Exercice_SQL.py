a9263ae8de109dff4b7fdd3c166a0e73ff32b7e5
AmeSansCible <racli_raclette@hotmail.com>
2024-11-25 13:02:41 +0100

    Run sans erreur

--- Exercice_SQL.py
+++ Exercice_SQL.py
@@ -86,14 +86,18 @@ create_requirements = """
 INSERT INTO
     requirements (level, row, column, seat)
 VALUES
-    ();
+    ('good', 'A', '1', 'left'),
+    ('medium', 'B', '2', 'right'),
+    ('bad', 'C', '3', 'middle');
 """
 
 create_desks = """
 INSERT INTO
     desks (row, column, seat)
 VALUES
-    ();
+    (1, 1, 1),
+    (1, 2, 1),
+    (2, 1, 1);
 """
 
 
@@ -122,8 +126,8 @@ students = execute_read_query(connection, select_students)
 for student in students:
     print(student)
 
-#Sélectionner 1 valeur précise (FONCTIONNE PAS)
-select_students = "SELECT student FROM students WHERE id = 2"
+#Sélectionner 1 valeur précise
+select_students = "SELECT first_name, name FROM students WHERE id = 2"
 students = execute_read_query(connection, select_students)
 
 for student in students:
@@ -134,12 +138,56 @@ update_students = """
 UPDATE 
     students
 SET
-    student = "Enzo"
+    first_name = "Enzo"
 WHERE
     id = 2
 """
 
 execute_query(connection, update_students)
 
-#Lier 2 tables ensemble
+#Lier 2 tables ensemble (relationnel) : table intermédiaire qui associe les élèves (students) aux niveaux (levels)
+#Les FOREIGN KEY permettent de relier les identifiants des étudiants (students.id) et des niveaux (levels.id).
+#Ajout d'une contrainte d’unicité (PRIMARY KEY) pour éviter les doublons
+create_student_levels_table = """
+CREATE TABLE student_levels (
+    student_id INTEGER UNIQUE,
+    level_id INTEGER,
+    PRIMARY KEY (student_id, level_id),
+    FOREIGN KEY (student_id) REFERENCES students (id),
+    FOREIGN KEY (level_id) REFERENCES levels (id)
+);
+"""
+execute_query(connection, create_student_levels_table)
+
+#Ajouter des données
+create_student_levels = """
+    INSERT INTO 
+        student_levels (student_id, level_id)
+    VALUES
+        (1, 1), -- Maeva a un bon niveau
+        (2, 2), -- Noémie a un niveau moyen
+        (3, 3); -- Nathan a un mauvais niveau
+"""
+execute_query(connection, create_student_levels)
+
+#Requête pour obtenir les étudiants avec leur level. Relie les données des étudiants (students) aux niveaux (levels) en passant par la table relationnelle student_levels
+
+select_students_with_levels = """
+SELECT 
+    students.first_name, students.name, levels.level -- récupère les colonnes first_name, name et level des tables students et levels
+FROM -- spécifie la table sur laquelle on travaille. Students est notre point de départ pour la jointure
+    students 
+INNER JOIN -- Associe chaque étudiant de students à son enregistrement dans student_levels.
+    student_levels ON students.id = student_levels.student_id
+INNER JOIN -- Associe chaque enregistrement de student_levels au niveau correspondant dans levels
+    levels ON student_levels.level_id = levels.id;
+"""
+# Si je rajoute la ligne de code WHERE levels.level = 'good'; cela me permettrait de récupérer les étudiants d'un level spécifique
+
+
+students_with_levels = execute_read_query(connection, select_students_with_levels)
+
+for student in students_with_levels:
+    print(student)
+
 
